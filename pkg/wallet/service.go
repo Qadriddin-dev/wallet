7caafc130a010cc067e1d6a295c56679f725ca0d
Qadriddin <qadrdin81@gmail.com>
2020-10-02 20:59:36 +0500

    _№_1

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -1,8 +1,9 @@
 package wallet
 
 import (
-  "errors"
-  "github.com/Qadriddin-dev/wallet/pkg/types"
+	"errors"
+
+	"github.com/Qadriddin-dev/wallet/pkg/types"
 )
 
 var ErrPhoneRegistered = errors.New("phone already registered")
@@ -12,54 +13,53 @@ var ErrNotEnoughBalance = errors.New("not enough balance in account")
 var ErrPaymentNotFound = errors.New("payment not found")
 
 type Service struct {
-  nextAccountID int64
-  accounts      []*types.Account
-  payments      []*types.Payment
+	nextAccountID int64
+	accounts      []*types.Account
+	payments      []*types.Payment
 }
 
 func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
-  for _, account := range s.accounts {
-    if account.Phone == phone {
-      return nil, ErrPhoneRegistered
-    }
-  }
-  s.nextAccountID++
-  account := &types.Account{
-    ID:      s.nextAccountID,
-    Phone:   phone,
-    Balance: 0,
-  }
-  s.accounts = append(s.accounts, account)
-  return account, nil
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, ErrPhoneRegistered
+		}
+	}
+	s.nextAccountID++
+	account := &types.Account{
+		ID:      s.nextAccountID,
+		Phone:   phone,
+		Balance: 0,
+	}
+	s.accounts = append(s.accounts, account)
+	return account, nil
 }
 
 func (s *Service) Deposit(accountID int64, amount types.Money) error {
-  if amount <= 0 {
-    return ErrAmountMustBePositive
-  }
-  var account *types.Account
-  for _, acc := range s.accounts {
-    if acc.ID == accountID {
-      account = acc
-      break
-    }
-  }
-
-  if account == nil {
-    return ErrAccountNotFound
-  }
-
-  account.Balance += amount
-  return nil
-}
-
-func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
+	if amount <= 0 {
+		return ErrAmountMustBePositive
+	}
 	var account *types.Account
 	for _, acc := range s.accounts {
 		if acc.ID == accountID {
 			account = acc
 			break
-		  }
+		}
 	}
-	return account, nil
+
+	if account == nil {
+		return ErrAccountNotFound
+	}
+
+	account.Balance += amount
+	return nil
+}
+
+func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
+
+	for _, account := range s.accounts {
+		if account.ID == accountID {
+			return account, nil
+		}
+	}
+	return nil, ErrAccountNotFound
 }
