a182a02ba0345559a34fabfb7b7479f41b51631e
Qadriddin <qadrdin81@gmail.com>
2020-10-03 09:07:58 +0500

    _#1_

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -1,36 +1,38 @@
 package wallet
+
 import (
 	"testing"
 )
+
 func TestService_RegisterAccount_success(t *testing.T) {
 	svc := Service{}
 	svc.RegisterAccount("+9920000001")
-  
+
 	account, err := svc.FindAccountByID(1)
 	if err != nil {
-	  t.Errorf("\ngot > %v \nwant > nil", account)
+		t.Errorf("\ngot > %v \nwant > nil", account)
 	}
-  }
-  
-  func TestService_FindAccountByID_success(t *testing.T) {
+}
+
+func TestService_FindAccountByID_success(t *testing.T) {
 	var service Service
 	service.RegisterAccount("9127660305")
-  
+
 	account, err := service.FindAccountByID(1)
-  
+
 	if err != nil {
-	  t.Errorf("account => %v", account)
+		t.Errorf("account => %v", account)
 	}
-  
-  }
-  func TestService_FindAccountByID_notFound(t *testing.T) {
+
+}
+func TestService_FindAccountByID_notFound(t *testing.T) {
 	var service Service
 	service.RegisterAccount("9127660305")
-  
+
 	account, err := service.FindAccountByID(2)
-  
+
 	if err == nil {
-	  t.Errorf("method returned nil error, account => %v", account)
+		t.Errorf("method returned nil error, account => %v", account)
 	}
-  
-  }
+
+}
