b93d17d81780f36fe2fa4229ef9020d1e943419d
Qadriddin <qadrdin81@gmail.com>
2020-10-03 11:43:45 +0500

    _2_

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,26 +1,39 @@
 package types
 
+
+// Money представляет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д.).
 type Money int64
 
+
+// PaymentCategory представляет собой категория в которой был совершён платёж (авто, аптеки, рестораны и т.д.).
 type PaymentCategory string
 
+// PaymentStatus представляет собой статус платежа.
 type PaymentStatus string
 
+
+// Предопределённые статусы платежей
 const (
-	PaymentStatusOk         PaymentStatus = "OK"
+	PaymentStatusOK         PaymentStatus = "OK"
 	PaymentStatusFail       PaymentStatus = "FAIL"
 	PaymentStatusInProgress PaymentStatus = "INPROGRESS"
 )
 
+
+// Payment представляет информацию о платеже. 
 type Payment struct {
-	ID       string
-	AmountID int64
-	Category PaymentCategory
-	Status   PaymentStatus
+	ID        string
+	AccountID int64
+	Amount    Money
+	Category  PaymentCategory
+	Status    PaymentStatus
 }
 
+// Phone представляет информацию о номере телефона пользователя.
 type Phone string
 
+
+// Account представляет информацию о счёте пользователя.
 type Account struct {
 	ID      int64
 	Phone   Phone
