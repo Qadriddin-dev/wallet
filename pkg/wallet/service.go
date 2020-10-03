8f3a54205e105875346b2b1984ef4551317a7287
Qadriddin <qadrdin81@gmail.com>
2020-10-03 18:36:48 +0500

    _2_

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -72,7 +72,7 @@ func (s *Service) Pay(accountID int64, amount types.Money, category types.Paymen
 	paymentID := uuid.New().String()
 	payment := &types.Payment{
 		ID:        paymentID,
-		AccountID: aссountID,
+		AccountID: accountID,
 		Amount:    amount,
 		Category:  category,
 		Status:    types.PaymentStatusInProgress,
